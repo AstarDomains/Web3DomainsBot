14e66e62f7eb465260aa212b3c14bdea9e330f0f
HerryRedstone <thachlan@outlook.com>
2022-03-16 13:49:18 +0700

    Create index.js

---
+++ DiscordBot/index.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const express = require('express');
+const url = require('url');  
+const querystring = require('querystring');
+const request = require('request');
+const Discord = require('discord.js');
